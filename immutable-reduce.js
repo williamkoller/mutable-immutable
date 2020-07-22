115820e05af890330c93a41fd37b78f9489aa3fc
William Koller <wkoller25@gmail.com>
2020-07-22 01:29:56 -0300

    feat: add reduce and for of

--- immutable-reduce.js
+++ immutable-reduce.js
@@ -1,12 +1,12 @@
 const twit = { x: 10, y: 20, z: 30 }
 
-Object.keys(twit).forEach(key => twit[key] *= 2)
+Object.keys(twit).forEach(key => twit[key])
 
 const updated = Object.entries(twit).reduce((obj, [key, value]) => {
     obj[key] = value *= 2
     return obj
 }, {})
 
-console.log(updated)
 console.log(twit)
+console.log(updated)
 
